ffdbb5c46d105f851ea686f753c75e54b23df327
Travis L. Seymour <nogard@ucsc.edu>
2025-02-09 10:42:44 -0800

    now allowing parens within square brace pattern

--- autoscore/safemath.py
+++ autoscore/safemath.py
@@ -44,3 +44,4 @@ if __name__ == "__main__":
     print(safe_eval_math("3 + 5 * (2 - 8)"))  # -25.0
     print(safe_eval_math("-4 / 2"))  # -2.0
     print(safe_eval_math("3.5 + 2.5"))  # 6.0
+    print(safe_eval_math("(100+75+75+75)/4"))  # 6.0
